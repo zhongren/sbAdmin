d4fffe8f31ea7f78f27814dcea7b43c286bcfcbd
zhongren <2674235651@qq.com>
2019-08-22 14:40:41 +0800

    整合mybatis

---
+++ src/main/java/com/example/demo/StartApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class StartApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+        log.info("启动完成");
+
+    }
+
+}
