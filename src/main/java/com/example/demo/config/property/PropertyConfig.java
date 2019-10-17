2f1b429bba6cc6e7ab2fd64c7fd6e99311a5b52e
zhongren <2674235651@qq.com>
2019-10-17 17:20:48 +0800

    优化代码

--- src/main/java/com/example/demo/config/property/PropertyConfig.java
+++ src/main/java/com/example/demo/config/property/PropertyConfig.java
@@ -1,20 +1,18 @@
-//package com.example.demo.config.property;
-//
-//import org.springframework.beans.MutablePropertyValues;
-//import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-//
-//@Configuration
-//public class PropertyConfig {
-//
-//    @Bean
-//    public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer(){
-//        PropertyPlaceholderConfigurer propertyPlaceholderConfigurer =new PropertyPlaceholderConfigurer();
-//        PathMatchingResourcePatternResolver resolver;
-//        MutablePropertyValues
-//
-//        return  propertyPlaceholderConfigurer;
-//    }
-//}
+package com.example.demo.config.property;
+
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration
+public class PropertyConfig {
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer=new PropertySourcesPlaceholderConfigurer();
+        propertySourcesPlaceholderConfigurer.
+    }
+}
